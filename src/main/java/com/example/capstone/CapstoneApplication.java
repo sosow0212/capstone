247c3ec9ba3a200ed1d0e217bef5328f342bb76d
sosow0212 <sosow0212@naver.com>
2022-11-07 21:26:15 +0900

    feat: aws.properties

--- src/main/java/com/example/capstone/CapstoneApplication.java
+++ src/main/java/com/example/capstone/CapstoneApplication.java
@@ -2,10 +2,12 @@ package com.example.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySource("classpath:/aws.properties")
 public class CapstoneApplication {
 
     public static void main(String[] args) {
