0917e594deb8c813862db8974d5b3921089a3eaf
sosow0212 <sosow0212@naver.com>
2022-11-13 21:14:51 +0900

    feat: Create RedisTemplate

--- src/main/java/com/example/capstone/config/RedisConfig.java
+++ src/main/java/com/example/capstone/config/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.example.capstone.config;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @PropertySource("classpath:secure.properties")
@@ -19,5 +22,13 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, List<String>> redisTemplate() {
+        RedisTemplate<String, List<String>> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
     }
 }
