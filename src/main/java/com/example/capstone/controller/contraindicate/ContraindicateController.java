3508412238c2e5f436c5f992c6a0c5c4ab3a4a54
sosow0212 <sosow0212@naver.com>
2022-11-15 09:28:28 +0900

    feat : Add ApiOperation in ContraindicateController

--- src/main/java/com/example/capstone/controller/contraindicate/ContraindicateController.java
+++ src/main/java/com/example/capstone/controller/contraindicate/ContraindicateController.java
@@ -3,16 +3,19 @@ package com.example.capstone.controller.contraindicate;
 import com.example.capstone.dto.contraindicate.RequestContraindicateDto;
 import com.example.capstone.response.Response;
 import com.example.capstone.service.contraindicate.ContraindicateService;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -22,6 +25,8 @@ public class ContraindicateController {
     private final ContraindicateService contraindicateService;
     private final RedisTemplate<String, List<String>> redisTemplate;
 
+    @ApiOperation(value = "병용금기 조회", notes = "병용금기 조회")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/contraindicate")
     public Response readContraindicate(@RequestParam("pill_a") String pill_a, @RequestParam("pill_b") String pill_b) {
         return Response.success(contraindicateService.findContraindicate(pill_a, pill_b));
