ad3c3dab13205031d306c552f37e0688dcfbbcb4
sosow0212 <sosow0212@naver.com>
2022-11-16 10:37:27 +0900

    test : Test findContraindicate, find data on redis test

--- src/test/java/com/example/capstone/service/ContraindicateServiceTest.java
+++ src/test/java/com/example/capstone/service/ContraindicateServiceTest.java
@@ -1,6 +1,6 @@
 package com.example.capstone.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.BDDMockito.given;
 
 import com.example.capstone.entity.contraindicate.Contraindicate;
@@ -9,42 +9,59 @@ import com.example.capstone.service.contraindicate.ContraindicateService;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ContraindicateServiceTest {
-    @InjectMocks
+
+    @Autowired
     ContraindicateService contraindicateService;
 
-    @Mock
+    @Autowired
     ContraindicateRepository contraindicateRepository;
 
-    @Mock
+    @Autowired
     RedisTemplate<String, String> redisTemplate;
 
     @Test
-    @DisplayName("병용금기 조회")
-    public void findContraindicateTest() {
+    @DisplayName("Redis 테스트")
+    public void redisTest() {
         // given
-//        ValueOperations<String, String> values = redisTemplate.opsForValue();
-//        String pillA = "pillA";
-//        String pillB = "pillB";
-//        Contraindicate contraindicate = new Contraindicate();
-//        contraindicate.setPillA(pillA);
-//        contraindicate.setPillB(pillB);
-//        contraindicate.setSymptom("테스트");
-//        given(contraindicateRepository.findByPillAAndPillB(pillA, pillB)).willReturn(Optional.of(contraindicate));
-//        given(values.get(pillA)).willReturn(contraindicate.getSymptom());
-//
-//        // when
-//        String result = contraindicateService.findContraindicate(pillA, pillB);
-//
-//        // then
-//        assertThat(result).isEqualTo("테스트");
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+
+        // when
+        hashOperations.put("key", "hashKey1", "val1");
+        hashOperations.put("key", "hashKey2", "val2");
+
+        // then
+        Object value1 = hashOperations.get("key", "hashKey1");
+        Object value2 = hashOperations.get("key", "hashKey2");
+
+        assertThat(hashOperations.get("key", "hashKey1")).isEqualTo(value1);
+        assertThat(hashOperations.get("key", "hashKey2")).isEqualTo(value2);
+
+        hashOperations.delete("key", "hashKey1");
+        hashOperations.delete("key", "hashKey2");
+    }
+
+    @Test
+    @DisplayName("findContraindicate 테스트 (레디스에 있는 경우)")
+    public void findContraindicateOnRedisTest() {
+        // given
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        String pillA = "testPillA";
+        String pillB = "testPillB";
+        String symptom = "testSymptom";
+        hashOperations.put(pillA, pillB, symptom);
+
+        // when
+        Object result = contraindicateService.findContraindicate(pillA, pillB);
+
+        // then
+        assertThat(result).isEqualTo(symptom);
+        hashOperations.delete(pillA, pillB);
     }
 }
