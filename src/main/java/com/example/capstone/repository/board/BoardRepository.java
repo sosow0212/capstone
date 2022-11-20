4d6591147c5ba330c7fb72c734623166307195cb
sosow0212 <sosow0212@naver.com>
2022-11-20 19:50:58 +0900

    del : remove board API

--- src/main/java/com/example/capstone/repository/board/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.capstone.repository.board;
-
-import com.example.capstone.entity.board.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
